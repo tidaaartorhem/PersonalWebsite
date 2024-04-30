2eb66e27f87580b9b1d6f0fcf95eb4fa939df889
tidaaartorhem <46531833+tidaaartorhem@users.noreply.github.com>
2024-04-29 20:09:50 -0400

    Update 29-April-Blogs

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/ResumeAaditFullTime.pdf";
+import pdf from "../../Assets/../Assets/Resume_Latest_Feb_1.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -33,7 +33,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file='../Assets/ResumeAadit.pdf' className="d-flex justify-content-center">
+          <Document file='../Assets/Resume_Latest_Feb_1.pdf' className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
